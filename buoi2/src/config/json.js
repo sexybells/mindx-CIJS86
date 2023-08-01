9155ef745b918eb1443467f796b91880fcf41042
Hải Dương <nguyenhaid98@yahoo.com>
2023-08-01 17:48:54 +0700

    buoi4

---
+++ buoi2/src/config/json.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        name: 'Product 1',
+        price: '$10',
+        description: 'Máy ảnh'
+    },
+    {
+        name: 'Product 2',
+        price: '$20',
+        description: 'Tàu'
+    },
+    {
+        name: 'Product 3',
+        price: '$30',
+        description: 'xe'
+    },
+    {
+        name: 'Product 4',
+        price: '$40',
+        description: 'cơm'
+    }
+]
